16fd2046a407dda2948eec9ddadd488f5bf6b9c6
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 04:55:49 +0545

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1>Makers </h1>    // ! jsx
+//       <h2>Bootcapm</h2>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return React.createElement(
+//     "div",
+//     { id: "makers", className: "Bootcamp" },
+//     React.createElement("h1", null, "Nakers Bootcamp")    // ! js
+//   );
+// }
+
+import React from "react";
+import Greet from "./components/Greet/Greet";
+
+const App = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greet name="Aibek" />
+      <Greet name="Ulan" />
+      <Greet name="Nursultan" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Greet/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Greet.css";
+const Greet = () => {
+  return (
+    <div className="greetings">
+      <h1>Hello Makers Bootcapm</h1>
+    </div>
+  );
+};
+
+export default Greet;
